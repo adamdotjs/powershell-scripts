6dcbf330137e8b163f09d56423c2ab7c5367231d
Adam Shea <avidworks@live.com>
2024-01-07 17:03:25 -0500

    add selection for imprint type

--- cjf.ps1
+++ cjf.ps1
@@ -1,14 +1,39 @@
 $sourceFolder = "C:\Users\avidw\Desktop\test"
-$jobNumber = Read-Host "Enter job number"
+$jobNumber = Read-Host -Prompt "Enter job number"
 $destination = "C:\Users\avidw\Desktop"
+$outputPath = Join-Path $destination $jobNumber
+[string[]]$imprintTypes = 'Digital', 'VDP', 'Inkjet', 'None'
 
-if (Test-Path $jobNumber) {
-    Write-Host "Folder '$jobNumber' already exists."
+Write-Host "Select imprint type:"
+for ($i=0; $i -lt $ImprintTypes.Count; $i++) {
+    Write-Host "$($i + 1). $($ImprintTypes[$i])"
+}
+
+$imprintType = Read-Host "Please choose the number associated with the imprint type needed."
+
+if (Test-Path $outputPath) {
+    Write-Host "Folder '$jobNumber' already exists." -ForegroundColor white -BackgroundColor red
     exit
 }
 
-$outputPath = Join-Path $destination $jobNumber
-
 Copy-Item -Path $sourceFolder -Destination $outputPath -Recurse -ErrorAction Stop
 
-Write-Host "Folder for job '$jobNumber' created successfully."
+switch($imprintType) {
+    1 { New-Item $outputPath -Name "vdp" -ItemType "directory" | Out-Null }
+    2 { New-Item $outputPath -Name "inkjet" -ItemType "directory" | Out-Null }
+    3 { New-Item $outputPath -Name "output" -ItemType "directory" | Out-Null }
+    4 { break }
+    default { Write-Host "Invalid option."}
+}
+
+Write-Host "You selected $imprintType"
+
+Write-Host "Folder for job '$jobNumber' created successfully." -ForegroundColor white -BackgroundColor blue
+
+$viewInExplorer = Read-Host -Prompt "View folder in Explorer? (Y/N)"
+
+if ($viewInExplorer -eq "Y") {
+    Invoke-Item $outputPath -ErrorAction Stop
+} else {
+    Write-Host "OK. You can close this window."
+}
